08d33bf638c70da1405cc20ab89db313f4430480
Lucas Marandat <lucas.mrdt@gmail.com>
2020-11-02 22:09:37 +0100

    add warning user agent

--- src/services/Form/screens/Instruction.screen.tsx
+++ src/services/Form/screens/Instruction.screen.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/jsx-pascal-case */
 
-import React from 'react';
-import { Button } from 'antd';
+import React, { useEffect } from 'react';
+import { Button, message } from 'antd';
 import styled from '@emotion/styled';
 import RightOutlined from '@ant-design/icons/RightOutlined';
 
@@ -46,50 +46,65 @@ interface Props {
   onSubmit: () => void;
 }
 
-const InstructionScreen: React.FC<Props> = ({ onSubmit }) => (
-  <>
-    <Container>
-      <Title>Générer votre attestation de déplacement COVID-19</Title>
-      <Headings.h2>
-        Votre attestation officielle en quelques secondes. Entrez vos
-        informations <b>une seule et unique fois</b>.
-      </Headings.h2>
-      <Steps>
-        <li>
-          <Paragraph>
-            Remplissez vos informations lors de votre première visite
-          </Paragraph>
-        </li>
-        <li>
-          <Paragraph>Choisissez le ou les motifs de déplacement</Paragraph>
-        </li>
-        <li>
-          <Paragraph>
-            Et voilà votre attestation de déplacement officielle COVID-19 est
-            prête !
-          </Paragraph>
-        </li>
-      </Steps>
-      <LegalNotice>
-        Toutes vos informations sont stockés localement à votre appareil et ne
-        sont jamais envoyé à un serveur. La web app est basée sur le projet
-        officiel du ministère de l'intérieur{' '}
-        <Anchor
-          href="https://media.interieur.gouv.fr/deplacement-covid-19/"
-          target="_blank"
-        >
-          COVID-19 ATTESTATION DE DÉPLACEMENT DÉROGATOIRE
-        </Anchor>
-        . Cette web app génère donc un document officiel de déplacement
-        COVID-19.
-      </LegalNotice>
-    </Container>
-    <StyledButton onClick={onSubmit}>
-      Commencer
-      <RightOutlined />
-    </StyledButton>
-  </>
-);
+const InstructionScreen: React.FC<Props> = ({ onSubmit }) => {
+  useEffect(() => {
+    message.warn({
+      content:
+        'Ouvrez le site sur votre navigateur préféré et non sur les réseaux sociaux.',
+      style: {
+        textAlign: 'left',
+      },
+    });
+  }, []);
+
+  return (
+    <>
+      <Container>
+        <Title>Générer votre attestation de déplacement COVID-19</Title>
+        <Headings.h2>
+          Votre attestation officielle en quelques secondes. Entrez vos
+          informations <b>une seule et unique fois</b>.
+          <br />
+          <br />
+          Utilisez votre navigateur préféré et non ceux des réseaux sociaux.
+        </Headings.h2>
+        <Steps>
+          <li>
+            <Paragraph>
+              Remplissez vos informations lors de votre première visite
+            </Paragraph>
+          </li>
+          <li>
+            <Paragraph>Choisissez le ou les motifs de déplacement</Paragraph>
+          </li>
+          <li>
+            <Paragraph>
+              Et voilà votre attestation de déplacement officielle COVID-19 est
+              prête !
+            </Paragraph>
+          </li>
+        </Steps>
+        <LegalNotice>
+          Toutes vos informations sont stockés localement à votre appareil et ne
+          sont jamais envoyé à un serveur. La web app est basée sur le projet
+          officiel du ministère de l'intérieur{' '}
+          <Anchor
+            href="https://media.interieur.gouv.fr/deplacement-covid-19/"
+            target="_blank"
+          >
+            COVID-19 ATTESTATION DE DÉPLACEMENT DÉROGATOIRE
+          </Anchor>
+          . Cette web app génère donc un document officiel de déplacement
+          COVID-19.
+        </LegalNotice>
+      </Container>
+      <StyledButton onClick={onSubmit}>
+        Commencer
+        <RightOutlined />
+      </StyledButton>
+    </>
+  );
+};
 
 // @ts-ignore
 InstructionScreen.whyDidYouRender = true;
