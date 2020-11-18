c65ed268e0dde4e3b17728459d7b526813a44c78
Lucas Marandat <lucas.mrdt@gmail.com>
2020-11-18 14:07:42 +0100

    test serviceWorker (2)

--- src/index.tsx
+++ src/index.tsx
@@ -23,7 +23,19 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
 serviceWorkerRegistration.register({
-  onUpdate: () => document.location.reload(),
+  onUpdate: (registration) => {
+    const waitingServiceWorker = registration.waiting;
+
+    if (waitingServiceWorker) {
+      waitingServiceWorker.addEventListener('statechange', (event) => {
+        // @ts-ignore
+        if (event?.target?.state === 'activated') {
+          window.location.reload();
+        }
+      });
+      waitingServiceWorker.postMessage({ type: 'SKIP_WAITING' });
+    }
+  },
 });
 
 // If you want to start measuring performance in your app, pass a function
